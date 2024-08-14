ddbae563dc425dc29248cf617d3a7a5e92c28a93
andr-d <andre.duewel@gmail.com>
2024-08-14 12:03:04 +0200

    retain permissions as octal int in AbstractAdapterDefinitionBuilder.php
    retain user provided file/dir permissions like 0775 or "0775" as octal int. Converting them with (int) yields incorrect values.

--- src/Adapter/Builder/AbstractAdapterDefinitionBuilder.php
+++ src/Adapter/Builder/AbstractAdapterDefinitionBuilder.php
@@ -70,12 +70,12 @@ abstract class AbstractAdapterDefinitionBuilder implements AdapterDefinitionBuil
             ->setFactory([PortableVisibilityConverter::class, 'fromArray'])
             ->addArgument([
                 'file' => [
-                    'public' => (int) $permissions['file']['public'],
-                    'private' => (int) $permissions['file']['private'],
+                    'public' => intval($permissions['file']['public'],8),
+                    'private' => intval($permissions['file']['private'],8),
                 ],
                 'dir' => [
-                    'public' => (int) $permissions['dir']['public'],
-                    'private' => (int) $permissions['dir']['private'],
+                    'public' => intval($permissions['dir']['public'], 8),
+                    'private' => intval($permissions['dir']['private'], 8),
                 ],
             ])
             ->addArgument($defaultVisibilityForDirectories)
